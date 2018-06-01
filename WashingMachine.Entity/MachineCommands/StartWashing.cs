d8d45023fa1a55fa62e6296734dc979b6eff8cc1
Adrián Vega <35713481+AdrianAVA9@users.noreply.github.com>
2018-05-31 18:53:56 -0600

    Last changes
    Last changes

---
+++ WashingMachine.Entity/MachineCommands/StartWashing.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace WashingMachine.Entity
+{
+    public class StartWashing: Command
+    {
+        public Washingmachine Machine { get; set; }
+
+        public StartWashing(Washingmachine machine)
+        {
+            Machine = machine;
+        }
+
+        public void SetWashingMachine(Washingmachine machine)
+        {
+            Machine = machine;
+        }
+
+        public override void Execute()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Execute(string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task ExecuteAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override async Task ExecuteAsync(string value)
+        {
+            await Machine.ToWash(value);
+        }
+    }
+}
--- WashingMachine.Entity/Program.cs
+++ WashingMachine.Entity/Program.cs
@@ -1,15 +1,124 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Runtime.CompilerServices;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace WashingMachine.Entity
 {
     class Program
     {
+        private delegate void InvokeDelegate();
+
         static void Main(string[] args)
         {
+            DateTime time;
+
+            time = DateTime.Now;
+            Console.WriteLine("Test 1");
+            Console.WriteLine(time);
+
+            time = DateTime.Today;
+            Console.WriteLine("Test 2");
+            Console.WriteLine(time);
+
+            Console.ReadKey();
+        }
+
+
+
+
+
+
+
+
+
+
+        static async Task NoMain()
+        {
+            AppDomain.CurrentDomain.UnhandledException += (s, e) => Log("*** Crash! ***", "UnhandledException");
+            TaskScheduler.UnobservedTaskException += (s, e) => Log("*** Crash! ***", "UnobservedTaskException");
+
+            await RunTests();
+
+            // Let async tasks complete...
+            Thread.Sleep(500);
+            GC.Collect(3, GCCollectionMode.Forced, true);
+        }
+
+        private static async Task RunTests()
+        {
+            try
+            {
+                // crash
+                // _1_VoidNoWait();
+
+                // crash 
+                // _2_AsyncVoidAwait();
+
+                // OK
+                // _3_AsyncVoidAwaitWithTry();
+
+                // crash - no await
+                // _4_TaskNoWait();
+
+                // crash - no await
+                // _5_TaskAwait();
+
+                // OK
+                // await _4_TaskNoWait();
+
+                // OK
+                await _5_TaskAwait();
+            }
+            catch (Exception ex) { Log("Exception handled OK");}
+            // crash - no try
+            // await _4_TaskNoWait();
+
+            // crash - no try
+            // await _5_TaskAwait();
+        }
+
+        // Unsafe
+        static void _1_VoidNoWait()
+        {
+            ThrowAsync();
+        }
+
+        // Unsafe
+        static async void _2_AsyncVoidAwait()
+        {
+            await ThrowAsync();
+        }
+
+        // Safe
+        static async void _3_AsyncVoidAwaitWithTry()
+        {
+            try { await ThrowAsync(); }
+            catch (Exception ex) { Log("Exception handled OK"); }
+        }
+
+        // Safe only if caller uses await (or Result) inside a try
+        static Task _4_TaskNoWait()
+        {
+            return ThrowAsync();
+        }
+
+        // Safe only if caller uses await (or Result) inside a try
+        static async Task _5_TaskAwait()
+        {
+            await ThrowAsync();
+        }
+
+        // Helper that sets an exception asnychronously
+        static Task ThrowAsync()
+        {
+            TaskCompletionSource<Task> tcs = new TaskCompletionSource<Task>();
+            ThreadPool.QueueUserWorkItem(_ => tcs.SetException(new Exception("ThrowAsync")));
+            return tcs.Task;
+        }
+        internal static void Log(string message, [CallerMemberName] string caller = "")
+        {
+            Console.WriteLine("{0}: {1}", caller, message);
         }
     }
 }
